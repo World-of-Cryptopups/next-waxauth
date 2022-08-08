d5e6d1d5e63d91406bc398de21fa1d997bc11216
TheBoringDude <iamcoderx@gmail.com>
2022-08-09 00:34:48 +0800

    export lib session functions for custom api purposes

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,4 @@
+import { createSession, getSession, removeSession } from "./auth";
 import UserSession from "./user";
 
-export { UserSession };
+export { UserSession, getSession, createSession, removeSession };
